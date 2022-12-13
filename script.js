485f02d27e5c103980aafa79d5b99c949e78bc8a
mtromano <70224285+mtromano@users.noreply.github.com>
2022-12-13 07:28:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 $(window).scroll(function(e){ 
-  var $el = $('.container-contents'); 
+  var $el = $('.container-title'); 
   var isPositionFixed = ($el.css('position') == 'fixed');
-  if ($(this).scrollTop() > 140 && !isPositionFixed){ 
+  if ($(this).scrollTop() > 60 && !isPositionFixed){ 
     $el.css({'position': 'fixed', 'top': '0px'}); 
   }
-  if ($(this).scrollTop() < 140 && isPositionFixed){
+  if ($(this).scrollTop() < 60 && isPositionFixed){
     $el.css({'position': 'static', 'top': '0px'}); 
   } 
 });
